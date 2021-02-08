f21a88cae8a2b8b038c6c255eb63022746161e5a
Macu <marconoelpgracela@yahoo.com>
2021-02-08 22:34:13 +0800

    Published first custom cra-templates: default and for typescript.

---
+++ packages/cra-template-custom/template/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <a
+          className="App-link"
+          href="https://github.com/ma-cu"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Press On and Focus Ahead Macu & Nica!
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ packages/cra-template-typescript-custom/template/src/App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <a
+          className="App-link"
+          href="https://github.com/ma-cu"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Focus and Press On Macu & Nica! 
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
